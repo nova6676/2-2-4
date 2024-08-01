155e5d4dfd449d457b48ed2950ac623102fc4a45
nova6676 <134915434+nova6676@users.noreply.github.com>
2024-08-02 00:31:47 +0400

    sdelano

--- PP_2_1_3_SpringBeanss/src/main/java/app/model/Animal.java
+++ PP_2_1_3_SpringBeanss/src/main/java/app/model/Animal.java
@@ -4,21 +4,5 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 public abstract class Animal {
-    @Component
-    public class Dog extends Animal {
-        @Autowired
-        private String dog;
 
-        public Dog(String dog) {
-            this.dog = dog;
-        }
-
-        public String getDog() {
-            return dog;
-        }
-
-        public void setDog(String dog) {
-            this.dog = dog;
-        }
-    }
 }
